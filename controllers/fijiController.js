4aac54e8ea756358e8222ac335d66a959fc906c3
Neelam Choudhary <neelam.choudhary@sjsu.edu>
2017-03-30 18:28:56 -0700

    added changes to get product image and checkbox to toggle between retail and wholesale price

--- controllers/fijiController.js
+++ controllers/fijiController.js
@@ -2,12 +2,18 @@
 	angular.module("fijiapp")
 		   .controller("fijiController",['$scope','$http',function ($scope,$http){
 		   
+
+
 		    $http({
 			  		method: 'GET',
 			 		url: 'http://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js'
 			 	}).then(function(response){
 					  	if (response.status == 200){	
 					  		//alert("200 ok")
+					  		$scope.wholesale = false;
+		  				    $scope.retail = true;
+		  				    $scope.discount = 1;
+			 				$scope.meta = response.data.products.meta;
 			 				$scope.products = response.data.products;
 						}else{
 							alert("404");
@@ -15,5 +21,13 @@
 
 					});
 
+		     $scope.changePrice = function (){
+				$scope.wholesale = true;
+				$scope.retail = false;
+				$scope.discount = .25;
+				}
+
+
 			}]);
 })();
+
